b16fcb66af4820bfd46505c91e05c1f195192e7c
nazi-k <nazik110@gmail.com>
2022-02-17 19:06:06 +0200

    del notify_for_start

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from . import misc
-from .notify_admins import on_startup_notify, bot_not_member_channel
+from .notify_admins import bot_not_member_channel
 from .checks_subscription import checks_subscription
