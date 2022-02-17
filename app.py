72d79cc43181e3c86d2b512a4b3b012400ad9c2c
nazi-k <nazik110@gmail.com>
2022-02-17 19:04:04 +0200

    del notify_for_start

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from aiogram.utils.executor import start_webhook
 
 from loader import dp
 import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
 from data.config import WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
@@ -11,9 +10,6 @@ from data.config import WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 async def on_startup(dispatcher):
     # Устанавливаем дефолтные команды
     await set_default_commands(dispatcher)
-
-    # Уведомляет про запуск
-    await on_startup_notify(dispatcher)
 
     await dp.bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -1,19 +1,10 @@
 import logging
 
-from aiogram import Dispatcher, Bot
+from aiogram import Bot
 
 from data.config import ADMINS
 
 from db.models import ChannelAd
-
-
-async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
-        try:
-            await dp.bot.send_message(admin, "Бот Запущений")
-
-        except Exception as err:
-            logging.exception(err)
 
 
 async def bot_not_member_channel(bot: Bot, channel: ChannelAd):
