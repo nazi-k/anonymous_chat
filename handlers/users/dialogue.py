49757acd3c77f5430e537908c7abb8ca8a580f48
nazi-k <nazik110@gmail.com>
2022-02-22 12:22:11 +0200

    del notify_for_start

--- handlers/users/dialogue.py
+++ handlers/users/dialogue.py
@@ -13,12 +13,12 @@ from dialogue.end import notify_you_skip
 from loader import dp
 
 
-@dp.message_handler(state=DialogueState.on_dialogue)
+@dp.message_handler(content_types=types.ContentTypes.all(), state=DialogueState.on_dialogue)
 @checks_subscription
 async def bot_dialogue(message: types.Message, session: AsyncSession, *args, **kwargs):
     dialogue = await Dialogue.get_dialogue_with_user(session, message.from_user.id)
     if dialogue:
-        if not message.text or not is_text_with_url(message.text):
+        if not is_message_with_url(message):
             await message.send_copy(dialogue.get_interlocutor(message.from_user.id))
         else:
             await notify_forbidden_message(message)
@@ -27,11 +27,18 @@ async def bot_dialogue(message: types.Message, session: AsyncSession, *args, **k
         await notify_you_skip(dp.bot, message.from_user.id)
 
 
-def is_text_with_url(text: str) -> bool:
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+" \
-            r"|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex, text)
-    return bool(url)
+def is_message_with_url(message: types.Message) -> bool:
+    if message["entities"]:
+        for message_entity in message["entities"]:
+            if message_entity["type"] == "url":
+                return True
+
+    if message["caption_entities"]:
+        for message_caption_entity in message["caption_entities"]:
+            if message_caption_entity["type"] == "url":
+                return True
+
+    return False
 
 
 async def notify_forbidden_message(message: types.Message) -> None:
